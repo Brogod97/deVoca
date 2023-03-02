c22942e4d8e49a12c4c953ba411864c3bfabf6c1
Brogod97 <evergreen1286@gmail.com>
2023-03-02 12:21:33 +0900

    template1 1차 완료, tempalte2 수정중

--- js/google-search.js
+++ js/google-search.js
@@ -1,21 +1,20 @@
-let query = document.querySelector(".query");
-const searchBtn = document.querySelector(".searchBtn");
-const googleSearchInput = document.getElementById("googleSearchInput");
+const googleSearchBtn = document.querySelector(".google-search-btn");
+const googleSearchInput = document.getElementById("google-search-input");
 
 // 구글 검색용 URL
 const queryUrl = "https://www.google.com/search?q=";
 
-// 검색어 포함 URL
-
 // 검색 버튼 클릭 시 이벤트
-searchBtn.addEventListener("click", function () {
-  let url = queryUrl + query.value;
+googleSearchBtn.addEventListener("click", function () {
+  let url = queryUrl + googleSearchInput.value;
   window.open(url);
+  googleSearchInput.value = "";
 });
 
 googleSearchInput.addEventListener("keyup", function () {
   if (window.event.keyCode == 13) {
-    let url = queryUrl + query.value;
+    let url = queryUrl + googleSearchInput.value;
     window.open(url);
+    this.value = "";
   }
 });
