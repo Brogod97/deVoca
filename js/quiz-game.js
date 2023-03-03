c139b615c1520e0a0f191ce19ea1014136c717bd
zinoah <zinoah@github.com>
2023-03-03 15:02:18 +0900

    member-update 이름 member-profile로 변경

--- js/quiz-game.js
+++ js/quiz-game.js
@@ -2,7 +2,7 @@ const quizProgress = document.getElementById("quiz-pro");
 const res = document.querySelector(".quiz-res");
 
 function skillUp() {
-    let qp = quizProgress.value++;
-    
-    res.innerText = qp + ' / 100';
+  let qp = quizProgress.value++;
+
+  res.innerText = qp + " / 100";
 }
