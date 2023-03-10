6d41186fa306cb7cc50efc9c0e500eafdf6c895c
Garam87 <dheotjd87@gmail.com>
2023-03-10 18:02:15 +0900

    랜딩페이지 퀴즈페이지 메인페이지(추가) 단어장페이지

--- js/quiz-game.js
+++ js/quiz-game.js
@@ -1,14 +1,3 @@
-const quizProgress = document.getElementById("quiz-pro");
-const res = document.querySelector(".quiz-res");
-
-function skillUp() {
-  let qp = quizProgress.value++;
-
-  res.innerText = qp + " / 100";
-}
-
-// --------
-
 const swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
@@ -19,3 +8,35 @@ const swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+swiper.once("click", function () {
+  if (swiper.isEnd) {
+    // 현재 슬라이드가 마지막 슬라이드인 경우
+    const link = document.createElement("a");
+    link.href = "/pages/quiz-result.html";
+
+    const btn = document.createElement("button");
+    btn.classList.add("btn-primary-fill");
+    btn.textContent = "결과보기";
+
+    link.appendChild(btn);
+
+    // 현재 활성화된 슬라이드에 a 태그 추가
+    const activeSlide = swiper.slides[swiper.activeIndex];
+    activeSlide.append(link);
+
+    // 화살표 엘리먼트의 pointer-events 속성 제거
+    const arrowPrev = document.querySelector(".swiper-button-prev");
+    const arrowNext = document.querySelector(".swiper-button-next");
+    arrowPrev.style.pointerEvents = "auto";
+    arrowNext.style.pointerEvents = "auto";
+  }
+});
+
+// 슬라이드 추가하는 함수
+// swiper.appendSlide('<div class="swiper-slide">Slide 10</div>');
+
+// appendSlide([
+//   '<div class="swiper-slide">Slide 10"</div>',
+//   '<div class="swiper-slide">Slide 11"</div>',
+// ]);
