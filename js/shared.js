25dae529f4d84a47cc333b9f6534e9cd9222159d
Brogod97 <evergreen1286@gmail.com>
2023-03-27 13:44:59 +0900

    shared 페이지에서 회원 목록 또는 카테고리 목록 클릭 시 해당 요소만 색상 활성화 되도록 설정

--- js/shared.js
+++ js/shared.js
@@ -4,11 +4,19 @@ const memberList = document.querySelectorAll(".member");
 
 memberList.forEach((member) => {
     member.addEventListener("click", function () {
-        // TODO: 선택된 member만 변경되도록 설정 필요 (jQuery의 siblings를 구현하고 싶음)
-        // https://aosceno.tistory.com/488
+        removeMemberListClassname(this, "backgroundPrimary");
         this.classList.add("backgroundPrimary");
     });
 });
+
+// MemberList에서 클릭한 요소 외 다른 형제 요소의 클래스 제거하는 함수
+function removeMemberListClassname(t, removeClass) {
+    const children = t.parentElement.children;
+
+    for (let i = 0; i < children.length; i++) {
+        children[i].classList.remove(removeClass);
+    }
+}
 
 // 카테고리 목록(shared-category-list)의 li(shared-category) 클릭 시 아이콘 컬러 변경
 // .active 추가 시 색상 활성화
@@ -16,8 +24,20 @@ const sharedCategoryList = document.querySelectorAll(".shared-category");
 
 sharedCategoryList.forEach((category) => {
     category.addEventListener("click", function () {
-        // TODO: 선택된 category만 변경되도록 설정 필요 (jQuery의 siblings를 구현하고 싶음)
-        // ic-hive-cc 접근
+        removeCategoryListClassname(
+            this.firstChild.nextElementSibling,
+            "active"
+        );
         this.firstChild.nextElementSibling.classList.add("active");
+        // this.firstChild.nextElementSibling.classList == ic-hive-cc 접근
     });
 });
+
+// CategoryList에서 클릭한 li의  클래스 제거하는 함수
+function removeCategoryListClassname(t, removeClass) {
+    const children = t.parentElement.parentElement.children;
+
+    for (let i = 0; i < children.length; i++) {
+        children[i].firstChild.nextSibling.classList.remove(removeClass);
+    }
+}
