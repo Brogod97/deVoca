80849b1f0004575c8203b2ad23bf68c164f7b10f
Garam87 <dheotjd87@gmail.com>
2023-03-30 14:59:13 +0900

    경로지정

---
+++ src/main/java/devoca/voca/controller/VocaQuizMainServlet.java
@@ -0,0 +1,21 @@
+package devoca.voca.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/voca/quiz-main")
+public class VocaQuizMainServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String path = "/WEB-INF/views/voca/quiz-main.jsp";
+		req.getRequestDispatcher(path).forward(req, resp);
+
+	}
+}
