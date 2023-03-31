9b2ce2f0057a976d24033ddf3d674892fadd0a07
Brogod97 <evergreen1286@gmail.com>
2023-03-31 14:11:01 +0900

    merge conflict solve

---
+++ src/main/java/devoca/voca/controller/SelectCategoryAllServlet.java
@@ -0,0 +1,36 @@
+package devoca.voca.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import devoca.voca.model.service.VocaService;
+import devoca.voca.model.vo.Category;
+
+@WebServlet("/voca/selectCategoryAll")
+public class SelectCategoryAllServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		VocaService service = new VocaService();
+		
+		int loginMemberNo;
+		List<Category> categoryList = null;
+		
+		
+		try {
+			
+			categoryList = service.selectCategoryAll();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
