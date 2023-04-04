b3e3144798b0ff0a149386d8eabbbb27686a696e
Brogod97 <evergreen1286@gmail.com>
2023-04-04 15:03:23 +0900

    병합 후 수정

--- src/main/java/devoca/member/controller/TestLoginServlet.java
+++ src/main/java/devoca/member/controller/TestLoginServlet.java
@@ -1,4 +1,4 @@
-/*package devoca.member.controller;
+package devoca.member.controller;
 
 import java.io.IOException;
 
@@ -63,4 +63,3 @@ public class TestLoginServlet extends HttpServlet{
 	}
 	
 }
-*/
