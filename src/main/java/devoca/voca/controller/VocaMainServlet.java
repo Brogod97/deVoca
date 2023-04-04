dcbb2bcfa94f68ff2b5e01fa163079718034ba09
zinoah <zinoah@github.com>
2023-04-04 14:35:01 +0900

    보카메인 서블릿 사이드바 카테고리 리스트 뿌리기

--- src/main/java/devoca/voca/controller/VocaMainServlet.java
+++ src/main/java/devoca/voca/controller/VocaMainServlet.java
@@ -1,6 +1,7 @@
 package devoca.voca.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,13 +10,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import devoca.member.model.vo.Member;
+import devoca.voca.model.service.VocaService;
+import devoca.voca.model.vo.Category;
+
 @WebServlet("/voca/voca-main")
 public class VocaMainServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String path = "/WEB-INF/views/voca/voca-main.jsp";
 		
-		req.getRequestDispatcher(path).forward(req, resp);
+		Member loginMember = (Member)req.getSession().getAttribute("loginMember");
+		int memberNo = loginMember.getMemberNo();
+	
+		try {
+			VocaService service = new VocaService();
+			
+			List<Category> category = service.selectCategoryAll(memberNo);
+			
+			
+			String path = "/WEB-INF/views/voca/voca-main.jsp";
+			req.getRequestDispatcher(path).forward(req, resp);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
+	
+	
 }
