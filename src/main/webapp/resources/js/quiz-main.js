f0d7826c54360d81e068521d7ee5a95cb13a6d63
Brogod97 <evergreen1286@gmail.com>
2023-04-06 12:34:14 +0900

    quiz-main 진입 시 실행되는 로딩 페이지 위치 수정

--- src/main/webapp/resources/js/quiz-main.js
+++ src/main/webapp/resources/js/quiz-main.js
@@ -1,28 +1,28 @@
 window.onload = function () {
-  showLoadingPage();
+    showLoadingPage();
 };
 
 function showLoadingPage() {
-  // 로딩 페이지를 화면에 보여줍니다.
-  document.querySelector(".wrap").style.display = "block";
-  document.querySelector(".container").style.display = "none";
-  document.querySelector(".container-main").style.display = "none";
+    // 로딩 페이지를 화면에 보여줍니다.
+    document.querySelector(".wrap").style.display = "block";
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".container-main").style.display = "none";
 
-  // 3초 후에 hideLoadingPage 함수를 호출합니다.
-  setTimeout(hideLoadingPage, 2000);
+    // 3초 후에 hideLoadingPage 함수를 호출합니다.
+    setTimeout(hideLoadingPage, 2000);
 }
 
 function hideLoadingPage() {
-  // 로딩 페이지를 화면에서 숨깁니다.
-  document.querySelector(".wrap").style.display = "none";
-  document.querySelector(".container").style.display = "flex";
-  document.querySelector(".container-main").style.display = "flex";
+    // 로딩 페이지를 화면에서 숨깁니다.
+    document.querySelector(".wrap").style.display = "none";
+    document.querySelector(".container").style.display = "flex";
+    document.querySelector(".container-main").style.display = "flex";
 }
 
 // 단어를 선택했을때 고정으로 색이 변하는 이벤트
 const content = document.querySelectorAll(".container-main-content > button");
 for (let i = 0; i < content.length; i++) {
-  content[i].addEventListener("click", (e) => {
-    e.target.style.backgroundColor = "#ffc759";
-  });
+    content[i].addEventListener("click", (e) => {
+        e.target.style.backgroundColor = "#ffc759";
+    });
 }
